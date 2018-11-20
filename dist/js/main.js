74c46a83f7e2b7d2169a8bb3d9cf2c8a15096c7f
Etien Cruz <etienc84@gmail.com>
2018-11-20 16:35:28 -0500

    Menu overlay.

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItem = document.querySelectorAll(".nav-items");
+const navItem = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
 let showMenu = false;
@@ -22,7 +22,7 @@ function toggleMenu() {
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
-    menu.classList.add("show");
+    menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItem.forEach(item => item.classList.remove("show"));
